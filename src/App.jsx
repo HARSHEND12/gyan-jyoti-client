f06686b39e41abb996c7e814ec8e30adb2e29359
HARSHENDU_GIRI <155060594+HARSHEND12@users.noreply.github.com>
2025-08-14 19:34:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Gallery from "./components/Gallery";
 import Register from "./components/Register";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Gallery />
       <Register />
       <Contact />
+      <Footer/>
     </div>
   );
 }
